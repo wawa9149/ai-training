5d7029f756e290f32ca35cf0413fe3e5ee63bdca
wawa9149 <wawa9149@holamago.com>
2025-03-12 14:28:49 +0900

    test

--- wawa/models/resnet.py
+++ wawa/models/resnet.py
@@ -4,6 +4,6 @@ from torchvision.models import ResNet50_Weights
 
 def get_model():
     model = resnet50(weights=ResNet50_Weights.IMAGENET1K_V1) # 사전 학습된 모델 사용
-    model.conv1 = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1, bias=False)  # 입력 크기 맞춤
+    # model.conv1 = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1, bias=False)  # 입력 크기 맞춤
     model.fc = nn.Linear(2048, 10)  # MNIST는 10개의 클래스
     return model
